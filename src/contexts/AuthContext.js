7d2b09fb44ece2004c796a46cb1077a13d7bb938
Liam Mccluskey <liammail100@gmail.com>
2021-02-06 20:24:22 -0500

    added stuff

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState, useContext } from "react"
 import { auth } from "../firebase"
 import firebase from "firebase/app"
 import "firebase/auth"
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL
+})
 
 const AuthContext = React.createContext()
 
@@ -13,15 +18,34 @@ export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
     const [loading, setLoading] = useState(true)
 
-    function signUp(email, password) {
+    async function createUser(authResult, displayName) {
+        // Init user data in MongoDB after created in firebase
+        try {
+            const res = await api.post('/users', {
+                displayName: displayName,
+                firebaseUID: authResult.user.uid
+            })
+            // Did update user, store ._id in fb.auth.user.photoURL
+            if (res.data._id) {
+                await authResult.user.updateProfile({
+                    photoURL: res.data._id
+                })
+            }
+        } catch(error) { console.log(error) }
+    }
+
+
+    function signUp(displayName, email, password) {
     // Description: Standard register with email
         return auth.createUserWithEmailAndPassword(email, password)
+        .then( (userCredential) => createUser(userCredential, displayName) )
     }
 
     function continueWithGoogle() {
     // Description: Sign (In \ Up) with google pop up
         const provider = new firebase.auth.GoogleAuthProvider()
         return auth.signInWithPopup(provider)
+        .then( (userCredential) => createUser(userCredential, userCredential.user.displayName))
     }
 
     function signIn(email, password) {
