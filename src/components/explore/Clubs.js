7244014edfb94dff45bdb7ab4e8071ebbf23d9ea
Liam Mccluskey <liammail100@gmail.com>
2021-02-07 15:56:54 -0500

    added some features in /explore

--- src/components/explore/Clubs.js
+++ src/components/explore/Clubs.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ExploreHeader from './ExploreHeader'
 import MainHeader from '../headers/MainHeader'
+import axios from 'axios'
 
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL
+})
 
 export default function Clubs() {
+    const [clubs, setClubs] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await api.get('/clubs/search')
+                setClubs(res.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
 
     return (
-        <div>
-            <MainHeader />
-            <ExploreHeader subPath='/clubs' />
-            <h1>Clubs</h1>
+    <div>
+        <MainHeader />
+        <ExploreHeader subPath='/clubs' />
+        <div className='main-container'>
+            <div style={{margin: '40px 0px'}} className='d-flex jc-space-between ai-center'>
+                <input type='text' placeholder='Find a Club' />
+                <button className='solid-btn'>Create a Club</button>
+            </div>
+            {!clubs ? <p>Loading...</p> : 
+                <div>
+                    {clubs.map(club => 
+                        <div key={club._id} className='main-subcontainer' onClick={()=>console.log('clicked club')}>
+                            <h4>{club.name}</h4>
+                            <p>{`Number of Members: ${club.memberUIDs.length}`}</p>
+                        </div>
+                    )}
+                </div>
+            }
         </div>
+        
+    </div>
         
     )
 }
--- src/components/explore/Sessions.js
+++ src/components/explore/Sessions.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import MainHeader from '../headers/MainHeader'
 import ExploreHeader from './ExploreHeader'
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL
+})
 
 export default function Sessions() {
-    
+    const [sessions, setSessions] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await api.get('/sessions')
+                setSessions(res.data)
+            } catch(error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    })
     return (
         <div>
             <MainHeader />
             <ExploreHeader subPath='/sessions' />
-            <h1>Sessions</h1>
+            <div className='main-container'>
+                <div style={{margin: '40px 0px'}} className='d-flex jc-space-between ai-center'>
+                    <input type='text' placeholder='Find a Session' />
+                    <button className='solid-btn'>Create a Session</button>
+                </div>
+                {!sessions ? <p>Loading...</p> : 
+                    <div>
+                        {sessions.map(session => 
+                            <div key={session._id} className='main-subcontainer' onClick={()=>console.log('clicked session')}>
+                                <h4>{session.title}</h4>
+                                <p>{`Host Name: ${session.hostName}`}</p>
+                                <p>{`Members: ${session.members.map(m=>m.name).join(', ')}`}</p>
+                            </div>
+                        )}
+                    </div>
+                }
+            </div>
+            
         </div>
     )
 }
