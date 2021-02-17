a8fbe4a2a6ffac2fb0f1f36fb1af5518f95410c0
Liam Mccluskey <liammail100@gmail.com>
2021-02-16 21:52:24 -0500

    added tonza session functionality

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,38 +1,126 @@
 import React, {useEffect, useState} from "react"
 import MainHeader from "./headers/MainHeader"
+import SubHeader from './headers/SubHeader'
+import {useAuth} from '../contexts/AuthContext'
+import Loading from './misc/Loading'
 import axios from "axios"
+import {useParams} from 'react-router-dom'
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 export default function Session(props) {
-    const {params} = props.match
-    const [sessionData, setSessionData] = useState({})
+    const {sessionID} = useParams()
+    const {currentUser} = useAuth()
+    const [session, setSession] = useState(null)
+    const [activities, setActivities] = useState([])
+    const [myActivity, setMyActivity] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        async function fetchSession() {
-            const res = await api.get(`/sessions/${params.sessionID}`)
-            console.log(res.data)
-            setSessionData(res.data)
+        async function fetchData() {
+            await fetchSession()
+            await fetchActivities()
+            setLoading(false)
         }
-        fetchSession()
+        fetchData()
     }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            updateMyActivity()
+        }, 3000)
+    }, [myActivity])
+
+    async function fetchSession() {
+        try {
+            const res = await api.get(`/sessions/${sessionID}`)
+            setSession(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function fetchActivities() {
+        try {
+            const res = await api.get(`/sessions/${sessionID}/activities`)
+            setActivities(res.data)
+            console.log('my index')
+            console.log(res.data.findIndex(ac => ac.uid === currentUser.uid))
+            
+            setMyActivity(res.data[res.data.findIndex(ac => ac.uid === currentUser.uid)])
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function updateMyActivity() {
+        console.log('Trying to update your activity. Current status: \n')
+        console.log(myActivity)
+        if (!myActivity) { console.log('no activity'); return }
+        console.log('did update activity')
+        const random = () => {
+            return Math.floor(Math.random()*100) % 100 + 50
+        }
+        const temp = myActivity
+        temp.currentPace = random()
+        temp.averagePace = random()
+        temp.totalDistance = random()
+        try {
+            await api.patch(`/activities/${myActivity._id}`, temp)
+        } catch (error) {
+            console.log(error)
+        }
+        fetchActivities()
+
+    }
+
+    async function handleClickJoin() {
+        const activityData = {
+            name: currentUser.displayName,
+            uid: currentUser.uid
+        }
+        try {
+            const res = await api.post('/activities', activityData)
+            const sessionData = {
+                uid: currentUser.uid,
+                activityID: res.data._id
+            }
+            await api.patch(`/sessions/${sessionID}/join`, sessionData)
+            fetchActivities()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div>
             <MainHeader />
-            {!sessionData ? (
-                <h3>Loading...</h3>
-            ) : (
-                <div>
-                    <p>Session Host: {sessionData.hostName}</p>
-                    <p>Session ID: {sessionData._id}</p>
-                    <p>Session Start Time: {sessionData.startAt}</p>
+            {loading ? <Loading /> :
+            <div>
+                <SubHeader title={session.title} />
+                <div className='main-container'>
+                    <div className='d-flex jc-space-between ai-center'>
+                        <h3>Workout: 4 x 2k</h3>
+                        <button onClick={handleClickJoin} className='solid-btn'>Join</button>
+                    </div>
+                    <br />
+                    <div className='float-container'>
+                        {activities.map((ac, index) => (
+                            <div key={index} className='main-subcontainer  d-flex jc-space-between'>
+                                <h4>Name: {ac.name}</h4>
+                                <p>Current Pace: {ac.currentPace}</p>
+                                <p>Average Pace: {ac.averagePace}</p>
+                                <p>Total Meters: {ac.totalDistance}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-
-            )}
-            <h1></h1>
+            </div>
+            }
+            
         </div>
     )
 }
--- src/components/explore/Sessions.js
+++ src/components/explore/Sessions.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import MainHeader from '../headers/MainHeader'
 import ExploreHeader from './ExploreHeader'
+import Loading from '../misc/Loading'
 import axios from 'axios'
 import {useHistory} from 'react-router-dom'
 
@@ -10,6 +11,7 @@ const api = axios.create({
 
 export default function Sessions() {
     const [sessions, setSessions] = useState([])
+    const [loading, setLoading] = useState(true)
     const history = useHistory()
 
     useEffect(() => {
@@ -17,6 +19,7 @@ export default function Sessions() {
             try {
                 const res = await api.get('/sessions')
                 setSessions(res.data)
+                setLoading(false)
             } catch(error) {
                 console.log(error)
             }
@@ -28,29 +31,33 @@ export default function Sessions() {
         await api.patch(`/${sessionID}/join`)
         history.push(`/sessions/${sessionID}`)
     }
+
     return (
         <div>
             <MainHeader />
             <ExploreHeader subPath='/sessions' />
-            <div className='main-container'>
-                <div style={{margin: '40px 0px'}} className='d-flex jc-space-between ai-center'>
-                    <input type='text' placeholder='Find a Session' />
-                    <button onClickclassName='solid-btn'>Create a Session</button>
-                </div>
-                {!sessions ? <p>Loading...</p> : 
-                    <div>
-                        {sessions.map(session => 
-                            <div key={session._id} className='main-subcontainer' onClick={()=>console.log('clicked session')}>
-                                <div className='d-flex jc-space-between ai-center'>
-                                    <h4>{session.title}</h4>
-                                    <p>{`${session.members.length}  Members`}</p>
-                                </div>
-                                <p>{`Host Name: ${session.hostName}`}</p>
-                            </div>
-                        )}
+            {loading ? <Loading /> :
+                <div className='main-container'>
+                    <div style={{margin: '40px 0px'}} className='d-flex jc-space-between ai-center'>
+                        <input type='text' placeholder='Find a Session' />
+                        <button className='solid-btn'>Create a Session</button>
                     </div>
-                }
-            </div>
+                    {!sessions ? <p>Loading...</p> : 
+                        <div>
+                            {sessions && sessions.map(session => 
+                                <div key={session._id} className='main-subcontainer' onClick={()=>console.log('clicked session')}>
+                                    <div className='d-flex jc-space-between ai-center'>
+                                        <h4>{session.title}</h4>
+                                        <p>{`${session.memberUIDs.length}  Members`}</p>
+                                    </div>
+                                    <p>{`Host Name: ${session.hostName}`}</p>
+                                </div>
+                            )}
+                        </div>
+                    }
+                </div>
+            }
+            
             
         </div>
     )
